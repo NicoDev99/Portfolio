5d7192e31f4e58c7d627434796ad154834872f8d
NicoDev99 <contact.nicopro.dev@gmail.com>
2024-04-29 23:54:35 +0200

    Remove failed import from tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-import ShootingStar from '@/components/ShootingStar.vue';
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],

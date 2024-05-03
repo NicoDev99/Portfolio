492c3d174b054242a1d8f155924ae9dab482f9b7
NicoDev99 <contact.nicopro.dev@gmail.com>
2024-05-03 14:03:12 +0200

    Changes before deployment

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes,
     scrollBehavior(to, from, savedPosition) {
         if(to.name == "CGU") {
